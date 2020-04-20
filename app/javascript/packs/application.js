be44e2fb6b6f3d66489b062852df694ba756ed5f
LamNNRuby <nguyenngoclamhipo@gmail.com>
2020-04-20 10:42:47 +0700

    updatetheme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+require("custom/bootstrap.min")
+require("custom/jquery.min")
+require("custom/jquery.zoom.min")
+require("custom/main")
+require("custom/nouislider.min")
+require("custom/slick.min")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
