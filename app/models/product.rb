d669f9179d21d33d74e7b76c4bd7f19e32f45fde
LamNNRuby <nguyenngoclamhipo@gmail.com>
2020-04-23 08:49:04 +0700

    update activeadmin 23-04

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category_pro
   has_one :product_detail
-
+  has_many :poll
   #show value name product in activeadmin
   def name
     return self.pro_name
