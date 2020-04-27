c97806f525396bf36626b381fef8d52416661850
LamNNRuby <nguyenngoclamhipo@gmail.com>
2020-04-27 20:49:05 +0700

    update t2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,12 @@
 class Product < ApplicationRecord
   validates :quantity, :price_input, :price_output, numericality: true
-  validates :price_output, numericality:  {greater_than_or_equal_to: :price_input}
   validates :pro_name, :quantity, :price_input, presence: true
+  validates :price_output, numericality: { greater_than_or_equal_to: :price_input }
   validates :pro_name, :uniqueness => true
-  validates :img_1, :img_2, :img_3, :format => {
-    :with => %r{\.(gif|jpg|png)\Z}i,
-    :message => 'Chi nhan file GIF, JPG, PNG'
-}
+  validates :img_1, :img_3, :img_2, allow_blank: true, format: {
+                          with: %r{\.(gif|jpg|png)\Z}i,
+                          :message => "Chi nhan file GIF, JPG, PNG",
+                        }
   belongs_to :category_pro
   has_one :product_detail
   has_many :poll
@@ -14,10 +14,9 @@ class Product < ApplicationRecord
   mount_uploader :img_1, ImageUploader
   mount_uploader :img_2, ImageUploader
   mount_uploader :img_3, ImageUploader
-  
+
   #show value name product in activeadmin
   def name
     return self.pro_name
   end
-  
 end
