992e5974d9281fcf8d9bb72f7bf04170fc4fbab1
nguyenquocbao28 <nguyenquocbao759@gmail.com>
2020-05-08 17:01:07 +0700

    Check-out update

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -4,10 +4,10 @@ module CurrentCart
   private
 
   def set_cart
-    #   @cart = Cart.find(session[:cart_id])
-    # rescue ActiveRecord::RecordNotFound
-    #   @cart = Cart.create
-    #   session[:cart_id] = @cart.id
+      #   @cart = Cart.find(session[:cart_id])
+      # rescue ActiveRecord::RecordNotFound
+      #   @cart = Cart.create
+      #   session[:cart_id] = @cart.id
     if user_signed_in?
       @cart = current_user.cart || current_user.create_cart
       session[:cart_id] = @cart.id
