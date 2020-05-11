164fc79bd23cc2e292fdd20a37ffb098fa8353fc
Linh Ruby <qlinh1080@gmail.com>
2020-05-11 14:31:40 +0700

    phantrang_show_category_product_11_05

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,27 @@ class ProductsController < InheritedResources::Base
   # load_and_authorize_resource
   # before_action :search_product
 
-  def show
-    @products1 = Product.all
+  def index
+    if params[:category_pro_id].present?
+      @product = Product.order("created_at desc").includes(:category_pro).where(category_pro_id: params[:category_pro_id]).paginate(:page => params[:page], :per_page => 9)
+
+      # @arr_name = CategoryPro.includes(:product).where(id: params[:category_pro_id]).map do |pro| { name: pro.category_name } end
+
+      # @category_name = CategoryPro.includes(:product).where(id: params[:category_pro_id]).map do |pro| { name: pro.category_name } end.map { |x| print x[:name] }
+
+      @category_nav = params[:category_name]
+    else
+      @category_nav = "All"
+      @product = Product.all.paginate(:page => params[:page], :per_page => 9)
+    end
+    # @orders = Order.paginate(:page => params[:page], :per_page => 10).order("created_at desc")
     search_product
-    @product = Product.includes(:product_detail).find(params[:id])
+  end
+
+  def show
+    # @products = Product.all
+    # search_product
+    @products = Product.includes(:product_detail).find(params[:id])
     @poll = Product.includes(:poll).find(params[:id])
     sum_vote
   end
@@ -45,7 +62,7 @@ class ProductsController < InheritedResources::Base
     @search = params["search"]
     if @search.present?
       name = @search
-      @products1 = Product.where("pro_name like ?", "%#{name}%")
+      @product = Product.where("pro_name like ?", "%#{name}%")
     end
   end
 
