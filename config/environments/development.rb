7534e432a0136612a6d438aadc933e402161612b
LamNNRuby <nguyenngoclamhipo@gmail.com>
2020-05-14 00:34:02 +0700

    mail server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,12 +2,12 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = {  host: 'vast-caverns-55328.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      'nguyenngoclam3298@gmail.com',
     password:       '06031998kukenbia',
-    domain:         "domain.of.sender.net",
+    domain:         "herokuapp.com",
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: 'plain',
